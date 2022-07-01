68286aa5b45d45f31d1321c51e6d9f47368635bc
Meme <meme_es@hotmail.com>
2022-06-30 21:43:27 -0600

    Add base files for a .Net MVC project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace DotNetTransactions.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
